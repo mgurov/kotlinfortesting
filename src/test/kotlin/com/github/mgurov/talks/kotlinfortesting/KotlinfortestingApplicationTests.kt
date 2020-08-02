799d62341ff29458a266bd171755631f84bea3dc
Mykola Gurov <ngurov@gmail.com>
2020-08-02 18:02:47 +0200

    initial as per spring initializer

---
+++ src/test/kotlin/com/github/mgurov/talks/kotlinfortesting/KotlinfortestingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.mgurov.talks.kotlinfortesting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinfortestingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
